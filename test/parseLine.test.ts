792949360aff5968567a81549f64cef964e6974e
Charles Spencer Blancas <86479293+Charles-Spencer-Blancas@users.noreply.github.com>
2023-05-22 09:06:25 -0400

    Added more tests for parseLine

--- test/parseLine.test.ts
+++ test/parseLine.test.ts
@@ -6,14 +6,14 @@ import parseLine from '../src/parseLine';
 import exp from 'constants';
 
 describe('parseLine', () => {
-    it('should return null for parsedLine if the line is a comment so it starts with //', () => {
+    it('should return null if the line is a comment so it starts with //', () => {
         expect(parseLine('// this is a comment', [], 4)).toEqual({
             parsedLine: null,
             updatedTags: [],
         });
     });
 
-    it('should return null for parsedLine if the line is whitespace', () => {
+    it('should return null if the line is whitespace', () => {
         expect(parseLine('    ', [], 4)).toEqual({
             parsedLine: null,
             updatedTags: [],
@@ -116,5 +116,87 @@ describe('parseLine', () => {
                 '<div id="id" class="class1 class2" attr1="value1" attr2="value2">',
             updatedTags: ['div'],
         });
+    });
+
+    it('should return the correct html for a single tag with an inline string literal', () => {
+        expect(parseLine('div>{this is a string}', [], 4)).toEqual({
+            parsedLine: '<div>\nthis is a string',
+            updatedTags: ['div'],
+        });
+    });
+
+    it('should append the new tag to the list of tags if it is indented at a deeper level and is not self closing', () => {
+        expect(parseLine('    div', ['div'], 4)).toEqual({
+            parsedLine: '<div>',
+            updatedTags: ['div', 'div'],
+        });
+    });
+
+    it('should close the previous tag if the new tag is indented at the same level', () => {
+        expect(parseLine('div', ['div'], 4)).toEqual({
+            parsedLine: '</div>\n<div>',
+            updatedTags: ['div'],
+        });
+    });
+
+    it('should close the previous tag and the correct number of tags above it if the new tag is indented at a shallower level', () => {
+        expect(parseLine('div', ['div', 'div'], 4)).toEqual({
+            parsedLine: '</div>\n</div>\n<div>',
+            updatedTags: ['div'],
+        });
+
+        expect(parseLine('    div', ['div', 'div', 'div'], 4)).toEqual({
+            parsedLine: '</div>\n</div>\n<div>',
+            updatedTags: ['div', 'div'],
+        });
+    });
+
+    it('should properly write self closing tags (void elements)', () => {
+        const voidElements = [
+            'area',
+            'base',
+            'br',
+            'col',
+            'embed',
+            'hr',
+            'img',
+            'input',
+            'keygen',
+            'link',
+            'meta',
+            'param',
+            'source',
+            'track',
+            'wbr',
+            '!DOCTYPE', // This is not a tag but it is self closing
+        ];
+
+        voidElements.forEach((voidElement) => {
+            expect(parseLine(voidElement, [], 4)).toEqual({
+                parsedLine: `<${voidElement} />`,
+                updatedTags: [],
+            });
+        });
+    });
+
+    it('should properly write string literals', () => {
+        expect(parseLine('>{this is a string}', [], 4)).toEqual({
+            parsedLine: 'this is a string',
+            updatedTags: [],
+        });
+    });
+
+    it('should close previous tags if a string literal is indentented at a shallower level', () => {
+        expect(parseLine('>{this is a string}', ['div', 'div'], 4)).toEqual({
+            parsedLine: '</div>\n</div>\nthis is a string',
+            updatedTags: [],
+        });
+
+        expect(parseLine('    >{this is a string}', ['div', 'div'], 4)).toEqual(
+            {
+                parsedLine: '</div>\nthis is a string',
+                updatedTags: ['div'],
+            }
+        );
     });
 });
